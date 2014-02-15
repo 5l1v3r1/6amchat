98f1005c92369b6e1d1d83fd4348872585de687b
Matt Vleming <mattvleming@gmail.com>
2014-02-15 01:12:50 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,5 @@
 // Generated on 2014-02-14 using generator-angularexpress 0.0.5
 'use strict';
-var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
@@ -16,35 +9,24 @@ module.exports = function (grunt) {
     yeoman: {
       // configurable paths
       app: require('./bower.json').appPath || 'app',
-      dist: 'dist'
+      dist: 'dist',
+      server: 'server'
     },
     watch: {
-      coffee: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.coffee'],
-        tasks: ['coffee:dist']
-      },
-      coffeeTest: {
-        files: ['test/spec/{,*/}*.coffee'],
-        tasks: ['coffee:test']
-      },
       jade: {
         files: ['<%= yeoman.app %>/jade/**/*.jade'],
         tasks: ['jade']
       },
       styles: {
-        files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
-        tasks: ['copy:styles', 'autoprefixer']
+        files: ['<%= yeoman.app %>/styles/{,*/}*.scss'],
+        tasks: ['sass', 'autoprefixer']
       },
-      livereload: {
+      express: {
+        files: ['<%= yeoman.server %>/{,*/}*.js'],
+        tasks: ['express'],
         options: {
-          livereload: '<%= connect.options.livereload %>'
-        },
-        files: [
-          '<%= yeoman.app %>/{,*/}*.html',
-          '.tmp/styles/{,*/}*.css',
-          '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
-          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
+          spawn: false // Without this option specified express won't be reloaded
+        }
       }
     },
     autoprefixer: {
@@ -58,72 +40,6 @@ module.exports = function (grunt) {
         }]
       }
     },
-    connect: {
-      options: {
-        port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
-        livereload: 35729
-      },
-      proxies: [ // Local
-        {
-            context: '/api',
-            host: 'localhost',
-            port: 9002
-        }
-      ],
-      livereload: {
-        options: {
-          middleware: function (connect, options) {
-            var middlewares = [];
-            middlewares.push(proxySnippet);
-            options.base.forEach(function(base) {
-                // Serve static files.
-                middlewares.push(connect.static(base));
-              });
-            return middlewares;
-          },
-          open: true,
-          base: [
-            '.tmp',
-            '<%= yeoman.app %>'
-          ]
-        }
-      },
-      test: {
-        options: {
-          middleware: function (connect, options) {
-            var middlewares = [];
-            middlewares.push(proxySnippet);
-            options.base.forEach(function(base) {
-                // Serve static files.
-                middlewares.push(connect.static(base));
-              });
-            return middlewares;
-          },
-          port: 9001,
-          base: [
-            '.tmp',
-            'test',
-            '<%= yeoman.app %>'
-          ]
-        }
-      },
-      dist: {
-        options: {
-          middleware: function (connect, options) {
-            var middlewares = [];
-            middlewares.push(proxySnippet);
-            options.base.forEach(function(base) {
-                // Serve static files.
-                middlewares.push(connect.static(base));
-              });
-            return middlewares;
-          },
-          base: '<%= yeoman.dist %>'
-        }
-      }
-    },
     clean: {
       dist: {
         files: [{
@@ -135,7 +51,7 @@ module.exports = function (grunt) {
           ]
         }]
       },
-      server: '.tmp'
+      server: ['.tmp', '.sass-cache']
     },
     jshint: {
       options: {
@@ -147,35 +63,6 @@ module.exports = function (grunt) {
         '<%= yeoman.app %>/scripts/{,*/}*.js'
       ]
     },
-    coffee: {
-      options: {
-        sourceMap: true,
-        sourceRoot: ''
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/scripts',
-          src: '{,*/}*.coffee',
-          dest: '.tmp/scripts',
-          ext: '.js'
-        }]
-      },
-      test: {
-        files: [{
-          expand: true,
-          cwd: 'test/spec',
-          src: '{,*/}*.coffee',
-          dest: '.tmp/spec',
-          ext: '.js'
-        }]
-      }
-    },
-    // not used since Uglify task does concat,
-    // but still available if needed
-    /*concat: {
-      dist: {}
-    },*/
     rev: {
       dist: {
         files: {
@@ -188,6 +75,19 @@ module.exports = function (grunt) {
         }
       }
     },
+    sass: [{
+      options: {
+        loadPath: ['<%= yeoman.app %>/bower_components/foundation/scss', '<%= yeoman.app %>/bower_components'],
+        sourcemap: true
+      },
+      files: [{
+        expand: true,
+        cwd: '<%= yeoman.app %>/styles',
+        src: '{,*/}*.scss',
+        dest: '.tmp/styles/',
+        ext: '.css'
+      }]
+    }],
     jade: {
       index: {
         files: {
@@ -195,16 +95,6 @@ module.exports = function (grunt) {
         },
         options: {
           basePath: '<%= yeoman.app %>/jade/',
-          client: false,
-          pretty: true
-        }
-      },
-      views: {
-        files: {
-          '<%= yeoman.app %>/views/': ['<%= yeoman.app %>/jade/views/**/*.jade']
-        },
-        options: {
-          basePath: '<%= yeoman.app %>/jade/views',
           client: false,
           pretty: true
         }
@@ -243,19 +133,6 @@ module.exports = function (grunt) {
         }]
       }
     },
-    cssmin: {
-      // By default, your `index.html` <!-- Usemin Block --> will take care of
-      // minification. This option is pre-configured if you do not wish to use
-      // Usemin blocks.
-      // dist: {
-      //   files: {
-      //     '<%= yeoman.dist %>/styles/main.css': [
-      //       '.tmp/styles/{,*/}*.css',
-      //       '<%= yeoman.app %>/styles/{,*/}*.css'
-      //     ]
-      //   }
-      // }
-    },
     htmlmin: {
       dist: {
         options: {
@@ -272,12 +149,11 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>',
-          src: ['*.html', 'views/*.html'],
+          src: '*.html',
           dest: '<%= yeoman.dist %>'
         }]
       }
     },
-    // Put files not handled in other tasks here
     copy: {
       dist: {
         files: [{
@@ -287,7 +163,6 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
             'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'fonts/*'
@@ -300,39 +175,20 @@ module.exports = function (grunt) {
             'generated/*'
           ]
         }]
-      },
-      styles: {
-        expand: true,
-        cwd: '<%= yeoman.app %>/styles',
-        dest: '.tmp/styles/',
-        src: '{,*/}*.css'
       }
     },
     concurrent: {
       server: [
         'jade',
-        'coffee:dist',
-        'copy:styles'
-      ],
-      test: [
-        'jade',
-        'coffee',
-        'copy:styles'
+        'sass'
       ],
       dist: [
         'jade',
-        'coffee',
-        'copy:styles',
+        'sass',
         'imagemin',
         'svgmin',
         'htmlmin'
       ]
-    },
-    karma: {
-      unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true
-      }
     },
     cdnify: {
       dist: {
@@ -358,39 +214,20 @@ module.exports = function (grunt) {
         }
       }
     },
-    express: {
-      custom: {
-        options: {
-          hostname: 'localhost',
-          port: 9002,
-          server: require('path').resolve('./server/main')
-        }
+    express: [{
+      options: {
+        script: './server/main.js',
+        debug: true
       }
-    }
+    }]
   });
 
-  grunt.registerTask('server', function (target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
-    }
-
-    grunt.task.run([
-      'clean:server',
-      'concurrent:server',
-      'configureProxies',
-      'express',
-      'autoprefixer',
-      'connect:livereload',
-      'watch'
-    ]);
-  });
-
-  grunt.registerTask('test', [
+  grunt.registerTask('server', [
     'clean:server',
-    'concurrent:test',
+    'concurrent:server',
+    'express',
     'autoprefixer',
-    'connect:test',
-    'karma'
+    'watch'
   ]);
 
   grunt.registerTask('build', [
@@ -398,7 +235,6 @@ module.exports = function (grunt) {
     'useminPrepare',
     'concurrent:dist',
     'autoprefixer',
-    'concat',
     'copy:dist',
     'cdnify',
     'ngmin',
@@ -410,7 +246,6 @@ module.exports = function (grunt) {
 
   grunt.registerTask('default', [
     'jshint',
-    'test',
     'build'
   ]);
 };
