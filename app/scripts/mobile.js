85910cfd192e24dc1ebf347032a9e79272a7034b
Matt Vleming <mattvleming@gmail.com>
2014-05-27 15:46:46 +0200

    Add slides to mobile.jade & design page

---
+++ app/scripts/mobile.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('.go-to-slide-1').click(function() {
+    $('.slide').addClass('js-hide');
+    $('#slide-1').removeClass('js-hide');
+  });
+
+  $('.go-to-slide-2').click(function() {
+    $('.slide').addClass('js-hide');
+    $('#slide-2').removeClass('js-hide');
+  });
+
+  $('.go-to-slide-3').click(function() {
+    $('.slide').addClass('js-hide');
+    $('#slide-3').removeClass('js-hide');
+  });
+});
